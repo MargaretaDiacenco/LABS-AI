9a4c8a7a827b788a77ea61ea3ed7cafdccc4093a
Margareta <margaretadiacenco@gmail.com>
2021-02-16 02:32:38 +0200

    creatures description

--- Lab 1/lab1.py
+++ Lab 1/lab1.py
@@ -1,3 +1,21 @@
 import random
 
-print("Hello world")
+#tourists database
+
+tourist1 = [0, 2, 8, 'human', 'combined', 'arachnimorph']
+tourist2 = [0, 2, 4, 'animal', 'oval', 'cowl']
+tourist3 = [0, 1, 0, 'none', 'sphere', 'floating_eyeball']
+tourist4 = [0, 2, 4, 'human', 'oval', 'gifany']
+tourist5 = [0, 16, 12, 'animal', 'combined', 'multibear']
+tourist6 = [0, 2, 4, 'none', 'oval', 'time_baby']
+tourist7 = [0, 0, 2, 'human', 'hexagon', 'hectorgon']
+alien = [0, 0, 2, 'animal', 'combined', 'loonie']
+
+
+attributes = ['sum', 'nr_of_eyes', 'nr_of_limbs', 'hair_type', 'body_shape']
+list_of_creatures = [tourist1, tourist2, tourist3, tourist4, tourist5, tourist6, alien]
+
+inputs = []
+
+#the system
+
